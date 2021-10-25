f54a0219633c3f2b304896eae075cc730fff52ac
Mio SUZUKI <sz.km.io.56@gmail.com>
2021-10-25 14:50:00 +0900

    Update PracticeA-Welcom-to-AtCoder.scm

--- PracticeA-Welcom-to-AtCoder.scm
+++ PracticeA-Welcom-to-AtCoder.scm
@@ -5,3 +5,11 @@
       (newline)
 )))
 (pracA)
+
+;; 手続きを作ることにこだわらないなら，こういう書き方もあるか
+;; 入力は4つとわかっているので，決め打ちで
+(define a (read))
+(define b (read))
+(define c (read))
+(define s (read))
+(print (+ a b c) " " s)
