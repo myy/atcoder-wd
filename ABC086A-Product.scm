ce56852e996ad01445844c9518e9e456f0442425
Mio SUZUKI <sz.km.io.56@gmail.com>
2021-10-25 14:50:35 +0900

    Update ABC086A-Product.scm

--- ABC086A-Product.scm
+++ ABC086A-Product.scm
@@ -3,3 +3,8 @@
     (let ((a (read)) (b (read)))
       (if (even? (* a b)) (print 'Even) (print 'Odd)))))
 (pracB)
+
+;; 別の書き方
+(define a (read))
+(define b (read))
+(print (if (odd? (* a b)) 'Odd 'Even ))
