54bf8cfaddbe4ca373b394251eacfa71df5689ca
Mio SUZUKI <sz.km.io.56@gmail.com>
2021-10-25 14:52:11 +0900

    Update ABC081A-Placing-Marbles.scm

--- ABC081A-Placing-Marbles.scm
+++ ABC081A-Placing-Marbles.scm
@@ -11,3 +11,17 @@
             (else (sub n 0)))
       )))
 (pracC)
+
+;; 別の書き方
+
+;; 最初に解いたときは，入力をそのまま数で扱って，各桁の数字が0か1かを確認していた
+;; 入力を3つに区切って，1の数をかぞえる，のほうがシンプルかな？と思って書いてみた．
+
+(define myfunc
+  (lambda (lis n)
+    (cond ((null? lis) n)
+          ((char=? (car lis) #\1) (myfunc (cdr lis) (+ n 1)))
+          (else (myfunc (cdr lis) n))
+          )
+    ))
+(myfunc (string->list (read-line)) 0) ;; (read-line)は入力を1行読み込んで，文字列にして返す
